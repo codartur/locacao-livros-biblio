6d48ada51cb2ea2f7161cbb620838f4f7f139553
Artur Cordeiro Duarte <arturcordeiroduarte.2@gmail.com>
2024-12-13 12:53:48 -0300

    Inicializando o projeto de caso de uso de locação

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, jsonify, request
+import json
+from datetime import datetime, timedelta
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+CORS(app)
+
+# Load data from JSON files
+def load_data(filename):
+    with open(filename, 'r') as f:
+        return json.load(f)
+
+def save_data(filename, data):
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=4)
+
+users = load_data('users.json')
+books = load_data('books.json')
+rentals = load_data('rentals.json')
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    return jsonify(users)
+
+@app.route('/books', methods=['GET'])
+def get_books():
+    return jsonify(books)
+
+@app.route('/rentals', methods=['GET', 'POST'])
+def handle_rentals():
+    if request.method == 'GET':
+        return jsonify(rentals)
+    elif request.method == 'POST':
+        rental = request.json
+        rentals.append(rental)
+        save_data('rentals.json', rentals)
+        return jsonify(rental), 201
+
+if __name__ == '__main__':
+    app.run(debug=True)
